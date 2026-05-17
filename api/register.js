b0475a818f6f5a424a560e90d19d20104483d263
lepem <166736840+lepem1@users.noreply.github.com>
2026-05-17 23:26:46 +0530

    Add MongoDB URI check and update database name

--- api/register.js
+++ api/register.js
@@ -1,7 +1,12 @@
 const { MongoClient } = require("mongodb");
 const bcrypt = require("bcryptjs");
 
-const client = new MongoClient(process.env.MONGODB_URI);
+if (!process.env.MONGODB_URI) {
+  throw new Error("MONGODB_URI missing");
+}
+
+const client =
+new MongoClient(process.env.MONGODB_URI);
 
 module.exports = async (req, res) => {
 
@@ -15,10 +20,13 @@ module.exports = async (req, res) => {
 
   await client.connect();
 
-  const db = client.db("rocard");
-  const users = db.collection("users");
+  const db = client.db("restorex");
 
-  const exists = await users.findOne({ email });
+  const users =
+  db.collection("users");
+
+  const exists =
+  await users.findOne({ email });
 
   if(exists){
     return res.json({
